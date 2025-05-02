22367761632fb4868af8b32d2d3d10ad9aee7d2a
hoccodedao <chillwith01@gmail.com>
2025-05-02 11:52:32 +0700

    First commit

---
+++ music-list.js
@@ -0,0 +1,93 @@
+let allMusic = [
+  {
+    name: "Em gái mưa",
+    artist: "Hương Tràm",
+    img: "b1",
+    src: "b1"
+  },
+ 
+  {
+    name: "Chạm đáy nỗi đau",
+    artist: "Mr.siro",
+    img: "b2",
+    src: "b2"
+  },
+  {
+    name: "Anh không tha thứ",
+    artist: "Đình Dũng",
+    img: "b3",
+    src: "b3"
+  },
+  {
+    name: "Ánh nắng của anh",
+    artist: "Đức Phúc ",
+    img: "b4",
+    src: "b4"
+  },
+  {
+    name: "Đi để trở về",
+    artist: "Soobin Hoàng Sơn",
+    img: "b5",
+    src: "b5"
+  },
+  {
+    name: "Xếp nỗi đau vào quá khứ",
+    artist: "Lâm Chấn Huy",
+    img: "b6",
+    src: "b6"
+  },
+  {
+    name: "Che Giấu Nỗi Đau",
+    artist: "Hoàng Ly",
+    img: "b7",
+    src: "b7"
+  },
+  {
+    name: "Lá Xa Lìa Cành",
+    artist: "Lê Bảo Bình",
+    img: "b8",
+    src: "b8"
+  },
+  {
+    name: "Thất Tình",
+    artist: "Trịnh Đình Quang",
+    img: "b9",
+    src: "b9"
+  },
+  {
+    name: "Có tất cả nhưng thiếu anh",
+    artist: "Mr.siro",
+    img: "b10",
+    src: "b10"
+  },
+  {
+    name: "Phía sau một cô gái",
+    artist: "Mr.siro",
+    img: "b11",
+    src: "b11"
+  },
+  {
+    name: "Ai là người thương em",
+    artist: "Quân A.P",
+    img: "b12",
+    src: "b12"
+  },
+  {
+    name: "Xin đừng lặng im",
+    artist: "Soobin Hoàng Sơn",
+    img: "b13",
+    src: "b13"
+  },
+  {
+    name: "Khó vẽ nụ cười",
+    artist: "Dat G",
+    img: "b14",
+    src: "b14"
+  },
+  {
+    name: "Nhìn em lần cuối",
+    artist: "Yuki Huy Nam",
+    img: "b15",
+    src: "b15"
+  },
+];
