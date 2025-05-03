b4416e8953429c9ef4f93d9cac452dcd2b196ae8
hoccodedao <chillwith01@gmail.com>
2025-05-03 13:52:32 +0700

    First commit

--- music-list.js
+++ music-list.js
@@ -90,4 +90,98 @@ let allMusic = [
     img: "b15",
     src: "b15"
   },
+  {
+    name: "Ngồi khóc cùng anh",
+    artist: "Hồ Quang Hiếu",
+    img: "b16",
+    src: "b16"
+  },
+  {
+    name: "Thay tôi yêu cô ấy",
+    artist: "Thanh Hưng ",
+    img: "b17",
+    src: "b17"
+  },
+  {
+    name: "Thu cuối",
+    artist: "Jet Remix",
+    img: "b18",
+    src: "b18"
+  },
+  {
+    name: "Em về đi em",
+    artist: "Hoa Vinh",
+    img: "b19",
+    src: "b19"
+  },
+  {
+    name: "Người lạ thoáng qua ",
+    artist: "Khởi My",
+    img: "b20",
+    src: "b20"
+  },
+  {
+    name: "Ngày em đẹp nhất",
+    artist: "Tama",
+    img: "b21",
+    src: "b21"
+  },
+  {
+    name: "Cẩm tú cầu",
+    artist: "Hương Ly",
+    img: "b22",
+    src: "b22"
+  },
+  {
+    name: "Đau để trưởng thành ",
+    artist: "Hương Ly Cover",
+    img: "b23",
+    src: "b23"
+  },
+  {
+    name: "Đơn Phương Mình Anh",
+    artist: "Linh Tuổi Thìn x LQ",
+    img: "b24",
+    src: "b24"
+  },
+  {
+    name: "Nước mắt em lau bằng tình yêu mới",
+    artist: "Da Lab",
+    img: "b25",
+    src: "b25"
+  },
+  {
+    name: "Hơn cả yêu",
+    artist: "Đức Phúc",
+    img: "b26",
+    src: "b26"
+  },
+  {
+    name: "Yêu ai cũng là sai mix người tính duyên trời",
+    artist: "Dj Viet Mix",
+    img: "b27",
+    src: "b27"
+  },
+  {
+    name: "Dưới Những cơn mưa",
+    artist: "Mr.Siro",
+    img: "b28",
+    src: "b28"
+  },
+
+  {
+  name : "Hôm nay em cưới rồi",
+  artist : "Khải Đăng",
+  img : "b29", 
+  src : "b29"
+  },
+  {
+    name : "Bánh mì không ft về",
+    artist : "Dj remix",
+    img : "b30", 
+    src : "b30"
+    },
+  
+
+
 ];
